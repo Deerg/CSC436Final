7e5333a0a2080918fc49b5aa9e69e4eece76184d
Daniel <danielzheng2@gmail.com>
2023-05-22 19:44:45 -0400

    Design

--- components/ActionsFooter.jsx
+++ components/ActionsFooter.jsx
@@ -4,33 +4,35 @@ import useUser from "csc-start/hooks/useUser";
 import Link from "next/link";
 import { getName } from "csc-start/utils/data";
 
-
 const ActionsFooter = () => {
   const [currentList, setCurrentList] = useState([]);
   const getNameByID = async () => {
     try {
-      const {data: tempCurrentList} = await getName(user.id);
+      const { data: tempCurrentList } = await getName(user.id);
       setCurrentList(tempCurrentList);
     } catch (error) {
       console.error(error);
     }
   };
-  useEffect(()=>{
+  useEffect(() => {
     getNameByID();
-  },);
+  });
 
   const { user, loading } = useUser();
   if (loading) {
     return <p className="barge">Loading</p>;
-  };
-
+  }
 
   if (!user) {
     // user is not logged in
     return (
       <div className="flex justify-between text-white mr-8">
-        <Link href="/login">Login</Link>
-        <Link href="/register">Register</Link>
+        <div className="m-5">
+          <Link href="/login">Login</Link>
+        </div>
+        <div className="m-5">
+          <Link href="/register">Register</Link>
+        </div>
       </div>
     );
   }
@@ -38,17 +40,22 @@ const ActionsFooter = () => {
   // user is logged in
   return (
     <div className="flex justify-center text-white">
-      <Link className="hover:text-brutal-yello duration-300 transition-all" href="/">
-      {currentList.map((item) => {
-                return (
-                  <a key={item.name}>
-                    {item.name}
-                  </a>
-                );
-              })}
+      <div className="m-5">
+        <Link
+          className="hover:text-brutal-yello duration-300 transition-all"
+          href="/"
+        >
+          {currentList.map((item) => {
+            return <a key={item.name}>{item.name}</a>;
+          })}
         </Link>
-      <Link href="/list/create">Create</Link>
-      <Link href="/logout">Logout</Link>
+      </div>
+      <div className="m-5">
+        <Link href="/list/create">Create</Link>
+      </div>
+      <div className="m-5">
+        <Link href="/logout">Logout</Link>
+      </div>
     </div>
   );
 };
